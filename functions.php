b160b0633888940387b891731685a7c5ee84d2f4
qwertyuiopngsdfg <pngsdfg@outlook.jp>
2018-04-24 14:27:44 +0000

    create function.php

---
+++ functions.php
@@ -0,0 +1,11 @@
+<?php
+
+class CreateDeck {
+    $numbers = array(1,2,3,4,5,6,7,8,9,10,11,12,13);
+    $Ranks = array('Clubs', 'Diamonds', 'Hearts', 'Spades');
+    foreach ($Ranks as $Rank) {
+        foreach ($numbers as $num) {
+            
+        }
+    }
+}
